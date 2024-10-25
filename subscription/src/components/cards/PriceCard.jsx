f1373a01897eefb9c4366bbaf71958fabf060203
Anandkrish7 <anandkick921@gmail.com>
2024-10-25 19:30:56 +0530

    UI fix 2

--- subscription/src/components/cards/PriceCard.jsx
+++ subscription/src/components/cards/PriceCard.jsx
@@ -3,13 +3,13 @@ const PriceCard = (props) => {
   return (
     <>
       <div className="col">
-        <div className="card mb-4 rounded-3 shadow-sm">
-          <div className="card-header py-3">
-            <h4 className="my-0 fw-normal">{plan}</h4>
+        <div className="card mb-4 rou shadow-sm">
+          <div className="plan-label">
+            <h5 style={{ color: "#fff" }}>{plan}</h5>
           </div>
 
           <div className="card-body">
-            <h1 className="card-title pricing-card-title">
+            <h2 className="card-title pricing-card-title">
               {!amount ? (
                 <>Your Active Plan</>
               ) : (
@@ -18,7 +18,7 @@ const PriceCard = (props) => {
                   <small className="text-muted fw-light">/mo</small>
                 </>
               )}
-            </h1>
+            </h2>
             <ul className="list-unstyled mt-3 mb-4">
               <li>{head_1}</li>
               <li>Email support</li>
@@ -27,7 +27,7 @@ const PriceCard = (props) => {
             <button
               style={{ cursor: "pointer" }}
               onClick={() => handleRequest(amount, plan, buttonTxt)}
-              className="w-100 btn btn-lg btn-outline-danger"
+              className="subscribe-button"
             >
               {buttonTxt}
             </button>
